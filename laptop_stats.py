45e3faeab5d3a04b6dd5c0af7eb8c84fb701ef06
Kanishka <Kanishka Dilshan>
2015-03-29 12:30:19 +0530

    Added support to monitor voltage and current

--- laptop_stats.py
+++ laptop_stats.py
@@ -13,8 +13,15 @@ def _temperature_stats():
 	core_1 = float(_read_from_file("/sys/class/hwmon/hwmon0/device/temp3_input"))/1000
 	return (board,core_0,core_1)
 
+def _power_stats():
+	capacity = float(_read_from_file("/sys/class/power_supply/BAT1/capacity"))
+	voltage = float( _read_from_file("/sys/class/power_supply/BAT1/voltage_now"))/10e5
+	current = float( _read_from_file("/sys/class/power_supply/BAT1/current_now"))/10e5
+	return (capacity,voltage,current)
+
 temp_stats = _temperature_stats()
-payload = {'api_key': 'TAAQHWLOGOP7RLO1','field1': temp_stats[0],'field2': temp_stats[1],'field3': temp_stats[2]}
+pows = _power_stats()
+payload = {'api_key': 'TAAQHWLOGOP7RLO1','field1': temp_stats[0],'field2': temp_stats[1],'field3': temp_stats[2],'field4': pows[0],'field5': pows[1],'field6': pows[2]}
 req = requests.post("http://api.thingspeak.com/update",payload)
 print req.text
 
